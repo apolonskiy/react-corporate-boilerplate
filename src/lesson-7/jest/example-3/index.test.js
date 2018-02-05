855a27fb5dfe4c52c5387550ccfff09aa3be397c
andriipolonskyi <andriy.polonskiy@gmail.com>
2018-02-05 14:10:20 +0200

    My commit

--- src/lesson-7/jest/example-3/index.test.js
+++ src/lesson-7/jest/example-3/index.test.js
@@ -2,6 +2,6 @@ import { add } from './';
 
 describe('jest/example-3 − \'add\' function:', () => {
     test('throws an error given a non-valid arguments', () => {
-        expect(() => add(false, NaN)).toThrow();
+        expect(() => add(false, NaN)).toThrow('An argument should be a number!');
     });
 });
